e4d5de33f5ca97eeb4e26f353b2205f4a3d98cd6
TranDucY <tranducy@tranducys-mac.local>
2018-08-15 00:10:43 -0700

    Initial Commit

---
+++ newProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newProject
+//
+//  Created by TranDucY on 8/15/18.
+//  Copyright © 2018 TranDucY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  newProject
+//
+//  Created by TranDucY on 8/15/18.
+//  Copyright © 2018 TranDucY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
